bedd47fc56c45a2f00a9ddded9595d97e7e4264c
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-06-26 00:57:38 -0300

    add shadcn tailwind and other packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+
+import Navbar from "@/components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <div className='m-4 flex min-h-screen flex-col items-end justify-end'> */}
+      <div className='p-4 w-full flex flex-col items-end justify-end'>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* </div> */}
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,124 @@
+"use client"
+
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Audios Neotel",
+    href: "/docs/primitives/hover-card",
+    description: "Pendiente de documentación por Ing. Bouzon.",
+  },
+  {
+    title: "Transcripción Manual",
+    href: "/",
+    description:
+      "Desde este módulo podrá transcribir cualquier audio que posea en su computadora.",
+  },
+  {
+    title: "Tareas",
+    href: "/",
+    description: "Visualice la lista de transcripciones pendientes.",
+  },
+  {
+    title: "Vistas",
+    href: "/",
+    description: "Pendiente de documentación por Ing. Bouzon.",
+  },
+]
+
+export default function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className='grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
+              <li className='row-span-3'>
+                <NavigationMenuLink asChild>
+                  <a
+                    className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
+                    href='/'
+                  >
+                    <div className='mb-2 text-lg font-medium'>AuditorIA</div>
+                    <p className='text-sm leading-tight text-muted-foreground'>
+                      Web app para visualizar transcripciones y análisis de
+                      sentimiento.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href='/docs' title='Introduction'>
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href='/docs/installation' title='Installation'>
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href='/docs/primitives/typography' title='Typography'>
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Utilidades</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
+              {components.map(component => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Documentation
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
