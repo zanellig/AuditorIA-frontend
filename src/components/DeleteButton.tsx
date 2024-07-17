ac621537f0f233a65cb718043b9bf16e521c84fc
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-07-17 17:25:22 -0300

    feat: permitir eliminación de uno o más ids

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -9,8 +9,15 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
+import { Task } from "@/lib/tasks"
 
-export default function DeleteButton({ id }: { id: string }) {
+export default function DeleteButton({
+  id,
+  ids,
+}: {
+  id?: Task["identifier"]
+  ids?: Task["identifier"][]
+}) {
   /**
    * TODO: Implement deletion API call
    * https://tasks.office.com/linksolution.com.ar/Home/Task/BYl9VJs82kec_NdGE_7RlWUAJYii?Type=TaskLink&Channel=Link&CreatedTime=638566702346980000
@@ -24,24 +31,30 @@ export default function DeleteButton({ id }: { id: string }) {
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>
-              Estás seguro que querés borrar la transcripción?
+              {ids
+                ? "Estás a punto de realizar una eliminación masiva"
+                : "¿Estás seguro que querés borrar la transcripción?"}
             </AlertDialogTitle>
             <AlertDialogDescription>
               Esta acción{" "}
               <span className='font-bold'>no se puede revertir</span>. Esto
               borrará{" "}
-              <span className='font-bold text-primary'>permanentemente</span> la
-              transcripción.
+              <span className='font-bold text-primary'>permanentemente</span>{" "}
+              {ids ? "las transcripciones seleccionadas" : "la transcripción"}.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogAction
               className='font-bold hover:text-destructive hover:outline-1 hover:-outline-offset-1 hover:outline hover:outline-destructive  hover:bg-transparent'
               onClick={() => {
-                console.log(id)
+                if (ids) {
+                  console.log(ids)
+                } else if (id) {
+                  console.log(id)
+                }
               }}
             >
-              Eliminar transcripción para siempre
+              Eliminar {ids ? "transcripciones" : "transcripción"} para siempre
             </AlertDialogAction>
             <AlertDialogCancel>Cancelar</AlertDialogCancel>
           </AlertDialogFooter>
