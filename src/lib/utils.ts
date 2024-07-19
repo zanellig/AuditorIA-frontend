bbba44db25c5e20df4a63a5808fc8821afb943f3
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-07-19 14:45:02 -0300

    fix: mover helper para mes a utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function obtenerMesLocale(mes: number): string {
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ]
+  return meses[mes]
+}
