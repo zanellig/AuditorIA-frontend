54c0012b720a762999e39b9ba0f1454f941bc15e
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-10-20 18:32:49 -0300

    fix(ci): Run Sentry only on production

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,13 +4,16 @@ import { env } from "@/env"
 
 export async function register() {
   console.log("Running in environment:", env.NODE_ENV)
-  if (env.NEXT_RUNTIME === "nodejs") {
-    await import("../sentry.server.config")
-  }
+  if (env.NODE_ENV === "production") {
+    if (env.NEXT_RUNTIME === "nodejs") {
+      await import("../sentry.server.config")
+    }
 
-  if (env.NEXT_RUNTIME === "edge") {
-    await import("../sentry.edge.config")
+    if (env.NEXT_RUNTIME === "edge") {
+      await import("../sentry.edge.config")
+    }
   }
 }
 
-export const onRequestError = Sentry.captureRequestError
+export const onRequestError =
+  env.NODE_ENV === "production" ? Sentry.captureRequestError : undefined
