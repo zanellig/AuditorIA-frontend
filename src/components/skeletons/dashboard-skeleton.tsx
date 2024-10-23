73d33745cf59e64993c55b850799dc2b24cb802b
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-10-23 12:06:55 -0300

    feat(ui): Upgrade table skeleton

--- src/components/skeletons/dashboard-skeleton.tsx
+++ src/components/skeletons/dashboard-skeleton.tsx
@@ -9,26 +9,35 @@ export default function DashboardSkeleton({
 }) {
   return (
     <section className={cn("w-full", className)}>
-      <div className='flex flex-col space-y-10'>
-        <div className='flex flex-row space-x-2 items-center w-full justify-between'>
-          <div className='flex flex-row space-x-4 items-center'>
-            <Skeleton className='h-8 rounded-xl w-[300px] bg-pulse dark:bg-secondary' />
-            <Skeleton className='h-6 rounded-xl w-[150px] bg-pulse dark:bg-secondary' />
+      <div className='flex flex-col space-y-4'>
+        <Skeleton className='w-full h-10 bg-pulse dark:bg-secondary' />
+        <div className='flex space-x-2 items-center w-full justify-between'>
+          <div className='flex space-x-4 items-center'>
+            <Skeleton className='h-8 w-[300px] bg-pulse dark:bg-secondary' />
+            <Skeleton className='h-8 w-[150px] bg-pulse dark:bg-secondary' />
+            <FilterButtonSkeleton />
+            <FilterButtonSkeleton />
+            <FilterButtonSkeleton />
           </div>
 
-          <div className='flex flex-row space-x-4 ml-auto'>
-            <Skeleton className='h-6 rounded-xl w-[200px] bg-pulse dark:bg-secondary' />
-            <Skeleton className='h-6 rounded-xl w-[100px] bg-pulse dark:bg-secondary' />
+          <div className='flex space-x-2 ml-auto'>
+            <Skeleton className='h-8 w-[200px] bg-pulse dark:bg-secondary' />
+            <Skeleton className='h-8 w-[100px] bg-pulse dark:bg-secondary' />
           </div>
         </div>
 
-        <div className='flex flex-col space-y-10'>
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
-          <RowSkeleton />
+        <div className='flex flex-col space-y-2'>
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
+          <SingleColumnSkeleton />
         </div>
 
         <PaginationSkeleton />
@@ -37,36 +46,24 @@ export default function DashboardSkeleton({
   )
 }
 
-function RowSkeleton() {
-  return (
-    <div className='space-x-4 flex flex-row'>
-      <SingleColumnSkeleton />
-      <SingleColumnSkeleton />
-      <SingleColumnSkeleton />
-      <SingleColumnSkeleton />
-      <SingleColumnSkeleton />
-      <SingleColumnSkeleton />
-      <SingleColumnSkeleton />
-      <SingleColumnSkeleton />
-    </div>
-  )
+function FilterButtonSkeleton() {
+  return <Skeleton className='h-8 w-10 bg-pulse dark:bg-secondary' />
 }
 
 function PaginationSkeleton() {
   return (
-    <div className='flex flex-row justify-between items-center'>
-      <Skeleton className='h-5 w-[500px] rounded-xl bg-pulse dark:bg-secondary' />
-      <Skeleton className='h-5 w-[200px] rounded-xl bg-pulse dark:bg-secondary' />
+    <div className='flex justify-between items-center'>
+      <Skeleton className='h-8 w-[500px] bg-pulse dark:bg-secondary' />
+      <Skeleton className='h-8 w-[200px] bg-pulse dark:bg-secondary' />
+      <div className='flex justify-between items-center gap-2'>
+        <Skeleton className='h-8 w-10 bg-pulse dark:bg-secondary' />
+        <Skeleton className='h-8 w-10 bg-pulse dark:bg-secondary' />
+        <Skeleton className='h-8 w-10 bg-pulse dark:bg-secondary' />
+      </div>
     </div>
   )
 }
 
 function SingleColumnSkeleton() {
-  return <Skeleton className='h-6 w-full bg-pulse dark:bg-secondary' />
-}
-
-function SinglePaginationButtonSkeleton() {
-  return (
-    <Skeleton className='h-5 rounded-full w-[40px] bg-pulse dark:bg-secondary' />
-  )
+  return <Skeleton className='h-10 w-full bg-pulse dark:bg-secondary' />
 }
