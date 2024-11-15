170fca483e98102192601dc8c88ea08559819242
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-11-15 15:24:14 -0300

    fix(audio): Fix error management

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -1,5 +1,4 @@
 // @/app/api/audio/route.ts
-import "server-only"
 import { type NextRequest, NextResponse } from "next/server"
 import { getNetworkAudio } from "@/lib/audio"
 import { getHeaders } from "@/lib/get-headers"
@@ -12,31 +11,35 @@ export async function OPTIONS(request: NextRequest) {
 }
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
   const headers = await getHeaders(request)
-  if (headers instanceof NextResponse) return headers
-  const path = searchParams.get("path")
-  if (!path) {
-    return new NextResponse(
-      JSON.stringify(new Error("Missing path parameter.")),
-      { status: 400, headers }
-    )
+  try {
+    const { searchParams } = new URL(request.url)
+    const path = searchParams.get("path")
+    if (!path) {
+      return NextResponse.json(new Error("Missing path parameter"), {
+        status: 400,
+        headers,
+      })
+    }
+    const [err, audioBuffer] = await getNetworkAudio(path)
+    if (audioBuffer === null) {
+      return NextResponse.json(new Error("No audio found in the path"), {
+        status: 404,
+        headers,
+      })
+    }
+    if (err !== null) {
+      throw new Error(err.message)
+    }
+    return new NextResponse(audioBuffer, {
+      headers: {
+        ...headers,
+        "Content-Type": "audio/mpeg",
+      },
+      status: 200,
+    })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return new NextResponse(error.message, { status: 500, headers })
   }
-  const [err, audioBuffer] = await getNetworkAudio(path)
-  if (audioBuffer === null) {
-    return new NextResponse(
-      JSON.stringify(new Error("No audio found in the path.")),
-      { status: 404, headers }
-    )
-  }
-  if (err !== null) {
-    return new NextResponse(err.message, { status: 500 })
-  }
-  return new NextResponse(audioBuffer, {
-    headers: {
-      ...headers,
-      "Content-Type": "audio/mpeg",
-    },
-    status: 200,
-  })
 }
