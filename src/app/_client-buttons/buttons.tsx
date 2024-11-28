6c60406175bea384b6075ee5aa8a85b3c723fcf6
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-11-28 02:58:33 -0300

    fix(ui): Fix login button to avoid overlaps *temporary

--- src/app/_client-buttons/buttons.tsx
+++ src/app/_client-buttons/buttons.tsx
@@ -1,18 +1,22 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import { ArrowRight } from "lucide-react"
+import { IPAD_SIZE_QUERY } from "@/lib/consts"
+import { useMediaQuery } from "@/lib/hooks/use-media-query"
+import { ArrowRight, LogIn } from "lucide-react"
 import Link from "next/link"
 
 export function LoginButton() {
+  const isDesktop = useMediaQuery(IPAD_SIZE_QUERY)
   return (
     <Button
       variant='expandIcon'
       Icon={ArrowRight}
       iconPlacement='right'
       className='bg-popover text-popover-foreground border border-border overflow-hidden'
+      size={!isDesktop ? "icon" : undefined}
     >
       <Link href='/login' className='w-full h-full'>
-        Iniciar Sesión
+        {isDesktop ? "Iniciar Sesión" : <LogIn />}
       </Link>
     </Button>
   )
