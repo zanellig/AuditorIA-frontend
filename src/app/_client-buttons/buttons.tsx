9f6c3f3e9d608f7c5500bcc21b31b00dddfe9913
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-11-28 03:03:54 -0300

    fix(ui): Fix Icon positioning for Login Button

--- src/app/_client-buttons/buttons.tsx
+++ src/app/_client-buttons/buttons.tsx
@@ -1,7 +1,8 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import { IPAD_SIZE_QUERY } from "@/lib/consts"
+import { GLOBAL_ICON_SIZE, IPAD_SIZE_QUERY } from "@/lib/consts"
 import { useMediaQuery } from "@/lib/hooks/use-media-query"
+import { cn } from "@/lib/utils"
 import { ArrowRight, LogIn } from "lucide-react"
 import Link from "next/link"
 
@@ -12,11 +13,14 @@ export function LoginButton() {
       variant={isDesktop ? "expandIcon" : "default"}
       Icon={ArrowRight}
       iconPlacement='right'
-      className='bg-popover text-popover-foreground border border-border overflow-hidden'
-      size={!isDesktop ? "icon" : undefined}
+      className={cn(
+        "bg-popover text-popover-foreground border border-border overflow-hidden",
+        !isDesktop && "text-foreground"
+      )}
+      size={!isDesktop ? "icon" : "default"}
     >
       <Link href='/login' className='w-full h-full'>
-        {isDesktop ? "Iniciar Sesión" : <LogIn />}
+        {isDesktop ? "Iniciar Sesión" : <LogIn size={GLOBAL_ICON_SIZE} />}
       </Link>
     </Button>
   )
