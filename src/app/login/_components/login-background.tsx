72da29083d2c9577da3d1a4e7498ee64fa985a90
zanellig <gonzalozanelli1@gmail.com>
2025-03-17 20:11:48 +0000

    refactor(login): Simplify login background component and update button structure
    - Remove unused imports and streamline the LoginBackground component
    - Update link structure for the "Aprendé más" button to enhance accessibility
    - Ensure consistent styling and functionality in the login background layout

--- src/app/login/_components/login-background.tsx
+++ src/app/login/_components/login-background.tsx
@@ -3,9 +3,7 @@ import Logo from "@/components/logo"
 import SubtitleH2 from "@/components/typography/subtitleH2"
 import { Button } from "@/components/ui/button"
 import { WavyBackground } from "@/components/ui/wavy-background"
-import { GLOBAL_ICON_SIZE } from "@/lib/consts"
 import { cn } from "@/lib/utils"
-import { ArrowDown } from "lucide-react"
 import Link from "next/link"
 
 export default function LoginBackground({ className }: { className?: string }) {
@@ -21,7 +19,7 @@ export default function LoginBackground({ className }: { className?: string }) {
         containerClassName='bg-background'
       >
         <div className='absolute top-0 right-0'>
-          <Link href='/' prefetch>
+          <Link href='/'>
             <div className='flex gap-4 items-center justify-center'>
               <Logo width={36} height={36} />
               <SubtitleH2 className='text-secondary dark:text-foreground'>
@@ -31,13 +29,14 @@ export default function LoginBackground({ className }: { className?: string }) {
           </Link>
         </div>
         <div className='absolute bottom-14 right-0'>
-          <Button
-            variant='outline'
-            className='flex gap-2 items-center w-fit justify-self-center'
-          >
-            <ArrowDown size={GLOBAL_ICON_SIZE} className='animate-bounce' />
-            <span>Aprendé más</span>
-          </Button>
+          <Link href='/'>
+            <Button
+              variant='outline'
+              className='flex gap-2 items-center w-fit justify-self-center'
+            >
+              Aprendé más
+            </Button>
+          </Link>
         </div>
       </WavyBackground>
     </section>
