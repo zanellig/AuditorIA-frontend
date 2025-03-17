8d0a751789bb3a015a247fdc6050df1f5d5c7600
zanellig <gonzalozanelli1@gmail.com>
2025-03-17 18:21:15 +0000

    refactor(loader): Replace Loader component with Loader2 and adjust icon size
    - Import GLOBAL_ICON_SIZE for consistent icon sizing
    - Update Loader component to Loader2 for improved visual representation
    - Maintain animation and color styling for dark and light themes

--- src/app/_loader/loader.tsx
+++ src/app/_loader/loader.tsx
@@ -1,9 +1,13 @@
-import { Loader } from "lucide-react"
+import { GLOBAL_ICON_SIZE } from "@/lib/consts"
+import { Loader2 } from "lucide-react"
 
 export default function LoaderScreen() {
   return (
     <main className='flex flex-col gap-2 w-full h-full justify-center items-center'>
-      <Loader className='h-20 w-20 animate-spin text-primary dark:text-secondary' />
+      <Loader2
+        size={GLOBAL_ICON_SIZE}
+        className='animate-spin text-primary dark:text-secondary'
+      />
     </main>
   )
 }
