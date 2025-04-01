dbf19de25b2d939c88d2a732209e23416a2c4940
zanellig <gonzalozanelli1@gmail.com>
2025-04-01 10:39:52 -0300

    fix(use-audit): Include username in audit query and request headers
    - Updated the useAudit hook to incorporate the user's username in the query key and as a header in the getAudit function, enhancing the audit data fetching process.

--- src/lib/hooks/use-audit.ts
+++ src/lib/hooks/use-audit.ts
@@ -1,3 +1,4 @@
+import { useUser } from "@/components/context/UserProvider"
 import { useQuery } from "@tanstack/react-query"
 
 export function useAudit({
@@ -7,17 +8,22 @@ export function useAudit({
   uuid: string
   enabled?: boolean
 }) {
+  const user = useUser()
   return useQuery({
-    queryKey: ["audit", uuid],
-    queryFn: () => getAudit(uuid),
+    queryKey: ["audit", uuid, user?.username],
+    queryFn: () => getAudit(uuid, user?.username),
     enabled: !!uuid && enabled,
   })
 }
 
-async function getAudit(uuid: string) {
+async function getAudit(uuid: string, username?: string) {
   const url = new URL(`http://10.20.62.96:5678/webhook/operator_quality`)
   url.searchParams.set("task_uuid", uuid)
-  const data = await fetch(url).then(async res => {
+  const data = await fetch(url, {
+    headers: {
+      "x-username": username ?? "",
+    },
+  }).then(async res => {
     if (!res.ok) {
       const data = await res.json()
       throw new Error(
