1c3c22e88135d48a7982c60f0a82f3ea80e7bed3
zanellig <gonzalozanelli1@gmail.com>
2025-04-01 11:33:39 -0300

    fix(loader): Update LoaderScreen styling for improved layout and animation
    - Adjusted the LoaderScreen component to ensure it occupies a minimum height of the screen, enhancing the visual layout.
    - Simplified the Loader2 component by removing the size prop while maintaining the spin animation for better performance.

--- src/app/_loader/loader.tsx
+++ src/app/_loader/loader.tsx
@@ -3,11 +3,8 @@ import { Loader2 } from "lucide-react"
 
 export default function LoaderScreen() {
   return (
-    <main className='flex flex-col gap-2 w-full h-full justify-center items-center'>
-      <Loader2
-        size={GLOBAL_ICON_SIZE}
-        className='animate-spin text-primary dark:text-secondary'
-      />
+    <main className='flex flex-col gap-2 w-full h-full min-h-screen justify-center items-center'>
+      <Loader2 className='animate-spin' />
     </main>
   )
 }
